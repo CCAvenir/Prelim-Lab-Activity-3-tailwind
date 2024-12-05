93c04a23335c6da47902de17522c0e5bafb6c988
christian avenir <avenirchristian22@gmail.com>
2024-12-05 13:16:09 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}"
+],
+theme: {
+  extend: {
+    colors: {
+      anubisBlue: '#1E40AF', // Custom theme color
+    },
+  },
+},
+  plugins: [],
+}
+
